122acdad9969d3831f215f8e5c9019eca2ac3a70
shinchu <cougousei@gmail.com>
2016-11-16 16:29:20 +0900

    [fix] minor fix

--- draw_161112.py
+++ draw_161112.py
@@ -1,85 +1,79 @@
 # drawbot 161112
 
-import random
-from sketch_helper import hexToRGB, drawGrid, drawPixel, saveSketch
+from random import shuffle, choice
+from sketch_helper import * 
 
+CANVAS = 500
 
-if __name__ == "__main__":
+PSIZE = 35
+NUMBERS = 10
 
-    width, height = 500, 500
+grey = hexToRGB("333333")
+white = hexToRGB("FFFFFF")
 
-    pixel_size = 35
-    numbers = 10
+red = hexToRGB("E6333E")
+magenda = hexToRGB("E5326B")
+orange = hexToRGB("ED7B26")
+cyan = hexToRGB("41AFC9")
+blue = hexToRGB("3096F0")
+purple = hexToRGB("A94CBB")
+green = hexToRGB("44A87E")
 
-    grey = hexToRGB("333333")
-    white = hexToRGB("FFFFFF")
+colors = [red, magenda, orange, cyan, blue, purple, green]
 
-    cream = hexToRGB("FEF3DB")
-    midnight = hexToRGB("353C5C")
+size(CANVAS, CANVAS)
 
-    red = hexToRGB("E6333E")
-    magenda = hexToRGB("E5326B")
-    orange = hexToRGB("ED7B26")
-    cyan = hexToRGB("41AFC9")
-    blue = hexToRGB("3096F0")
-    purple = hexToRGB("A94CBB")
-    green = hexToRGB("44A87E")
+fill(grey[0], grey[1], grey[2])
+rect(0, 0, CANVAS, CANVAS)
 
-    colors = [red, magenda, orange, cyan, blue, purple, green]
-    
-    size(width, height)
+stroke(white[0], white[1], white[2])
+fill(None)
+drawGrid(CANVAS, CANVAS, PSIZE, NUMBERS)
+
+pixel_addresses = [(4, 3), (5,3), (6, 3), (8, 3), (3, 4), (5, 4), (7, 4), (4, 5), (5, 5), (6, 5), (8, 5), (5, 6), (3, 7), (4, 7), (5, 7), (6, 7), (7, 7), (8, 7), (5, 8)]
+shuffle(pixel_addresses)
+
+DURATION = 1 / 30
+
+for i, address in enumerate(pixel_addresses):
+    newPage(CANVAS, CANVAS)
+
+    frameDuration(DURATION)
 
     fill(grey[0], grey[1], grey[2])
-    rect(0, 0, width, height)
+    rect(0, 0, CANVAS, CANVAS)
 
     stroke(white[0], white[1], white[2])
     fill(None)
-    drawGrid(width, height, pixel_size, numbers)
+    drawGrid(CANVAS, CANVAS, PSIZE, NUMBERS)
 
-    pixel_addresses = [(4, 3), (5,3), (6, 3), (8, 3), (3, 4), (5, 4), (7, 4), (4, 5), (5, 5), (6, 5), (8, 5), (5, 6), (3, 7), (4, 7), (5, 7), (6, 7), (7, 7), (8, 7), (5, 8)]
-    random.shuffle(pixel_addresses)
+    fill(white[0], white[1], white[2])
 
-    duration = 1 / 30
+    for j in range(i):
+        drawPixel(CANVAS, CANVAS, PSIZE, NUMBERS, pixel_addresses[j])
 
-    for i, address in enumerate(pixel_addresses):
-        newPage(width, height)
+    color = choice(colors)
+    fill(color[0], color[1], color[2])
+    drawPixel(CANVAS, CANVAS, PSIZE, NUMBERS, address)
 
-        frameDuration(duration)
+    newPage(CANVAS, CANVAS)
 
-        fill(grey[0], grey[1], grey[2])
-        rect(0, 0, width, height)
+    if i < len(pixel_addresses) - 1:
+        frameDuration(DURATION)
+    else:
+        frameDuration(DURATION * 20)
 
-        stroke(white[0], white[1], white[2])
-        fill(None)
-        drawGrid(width, height, pixel_size, numbers)
+    fill(grey[0], grey[1], grey[2])
+    rect(0, 0, CANVAS, CANVAS)
 
-        fill(white[0], white[1], white[2])
+    stroke(white[0], white[1], white[2])
+    fill(None)
+    drawGrid(CANVAS, CANVAS, PSIZE, NUMBERS)
 
-        for j in range(i):
-            drawPixel(width, height, pixel_size, numbers, pixel_addresses[j])
+    fill(white[0], white[1], white[2])
 
-        color = random.choice(colors)
-        fill(color[0], color[1], color[2])
-        drawPixel(width, height, pixel_size, numbers, address)
+    for j in range(i + 1):
+        drawPixel(CANVAS, CANVAS, PSIZE, NUMBERS, pixel_addresses[j])
 
-        newPage(width, height)
-
-        if i < len(pixel_addresses) - 1:
-            frameDuration(duration)
-        else:
-            frameDuration(duration * 20)
-
-        fill(grey[0], grey[1], grey[2])
-        rect(0, 0, width, height)
-
-        stroke(white[0], white[1], white[2])
-        fill(None)
-        drawGrid(width, height, pixel_size, numbers)
-
-        fill(white[0], white[1], white[2])
-
-        for j in range(i + 1):
-            drawPixel(width, height, pixel_size, numbers, pixel_addresses[j])
-
-    saveSketch("draw")
+saveSketch("draw")
 
