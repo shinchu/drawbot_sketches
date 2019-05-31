19806b020c0db752ad61ad18766d09172716d6e6
shinchu <cougousei@gmail.com>
2019-05-31 23:18:21 +0900

    Coding Math E11

---
+++ 1905/draw_190531a.py
@@ -0,0 +1,58 @@
+#! /usr/bin/env python
+# coding: utf-8
+# draw_190531a.py
+# 2019-05-31
+
+import os
+import sys
+
+mod_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../modules')
+if mod_dir not in sys.path:  
+    sys.path.append(mod_dir)
+
+import math
+import random
+import drawBot as db
+from vector import Vector
+from particle import Particle
+
+
+db.newDrawing()
+
+CANVAS = 500
+
+fps = 20
+sec = 5
+total_frames = fps * sec
+duration = 1 / fps
+
+sun = Particle(CANVAS / 2, CANVAS / 2, 0, 0)
+planet = Particle(CANVAS / 2 + 100, CANVAS / 2, 15, -math.pi / 2)
+
+sun.mass = 40000
+
+sun_r = 20
+planet_r = 5
+
+sun_d = sun_r * 2
+planet_d = planet_r * 2
+
+for frame in range(total_frames):
+    db.newPage(CANVAS, CANVAS)
+    db.frameDuration(duration)
+
+    planet.gravitate_to(sun)
+    planet.update()
+
+    db.stroke(0.8)
+    db.fill(0.8)
+
+    db.oval(sun.position.x - sun_r, sun.position.y - sun_r, sun_d, sun_d)
+    db.oval(planet.position.x - planet_r, planet.position.y - planet_r, planet_d, planet_d)
+    
+# Save image
+save_dir = os.path.expanduser('~/Downloads')
+file_name = 'temp.gif'
+db.saveImage(os.path.join(save_dir, file_name), imageResolution=144)
+
+db.endDrawing()
--- modules/particle.py
+++ modules/particle.py
@@ -3,15 +3,17 @@
 # particle.py
 # 2019-04-20
 #
+import math
 from vector import Vector
 
 class Particle:
-    def __init__(self, x, y, speed, direction, grav):
+    def __init__(self, x, y, speed, direction, grav=0):
         self.position = Vector(x, y)
         self.velocity = Vector(0, 0)
         self.velocity.set_length(speed)
         self.velocity.set_angle(direction)
         self.gravity = Vector(0, -grav) if grav else Vector(0, 0)
+        self.mass = 1
     
     def accelerate(self, accel):
         self.velocity.add_to(accel)
@@ -19,3 +21,21 @@ class Particle:
     def update(self):
         self.velocity.add_to(self.gravity)
         self.position.add_to(self.velocity)
+
+    def angle_to(self, p2):
+        return math.atan2(p2.position.y - self.position.y, p2.position.x - self.position.x)
+    
+    def distance_to(self, p2):
+        dx = p2.position.x - self.position.x
+        dy = p2.position.y - self.position.y
+
+        return math.sqrt(dx ** 2 + dy ** 2)
+    
+    def gravitate_to(self, p2):
+        grav = Vector(0, 0)
+        dist = self.distance_to(p2)
+
+        grav.set_length(p2.mass / (dist ** 2))
+        grav.set_angle(self.angle_to(p2))
+
+        self.velocity.add_to(grav)
