82b7c3b0ab778915fec107bb7ce24e448f4089a0
shinchu <cougousei@gmail.com>
2020-06-30 23:02:03 +0900

    Add new sketch: triangles

--- modules/vector.py
+++ modules/vector.py
@@ -14,14 +14,14 @@ class Vector:
 
     def __repr__(self):
         return 'Vector(x={}, y={}, length={}, angle={})'.format(self.x, self.y, self.length, self.angle)
-    
+
     def __str__(self):
         return '({}, {})'.format(self.x, self.y)
 
     @property
     def x(self):
         return self.__x
-    
+
     @property
     def y(self):
         return self.__y
@@ -51,47 +51,47 @@ class Vector:
     def angle(self, value):
         self.__angle = value
         self.update()
-    
+
     def update(self):
         self.x = math.cos(self.__angle) * self.__length
         self.y = math.sin(self.__angle) * self.__length
 
     def __add__(self, v2):
         return Vector(self.x + v2.x, self.y + v2.y)
-    
+
     def __sub__(self, v2):
         return Vector(self.x - v2.x, self.y - v2.y)
 
     def __mul__(self, val):
         return Vector(self.x * val, self.y * val)
-    
+
     def __div__(self, val):
         return Vector(self.x / val, self.y / val)
 
     def add(self, v2):
         return Vector(self.x + v2.x, self.y + v2.y)
-    
+
     def subtract(self, v2):
         return Vector(self.x - v2.x, self.y - v2.y)
 
     def multiply(self, val):
         return Vector(self.x * val, self.y * val)
-    
+
     def divide(self, val):
         return Vector(self.x / val, self.y / val)
 
     def add_to(self, v2):
         self.x += v2.x
         self.y += v2.y
-    
+
     def subtract_from(self, v2):
         self.x -= v2.x
         self.y -= v2.y
-    
+
     def multiply_by(self, val):
         self.x *= val
         self.y *= val
-    
+
     def divide_by(self, val):
         self.x /= val
         self.y /= val
