afda08609797e156b47b054de22ebc051fbe01b6
Shin Chu <shushinjo@gmail.com>
2021-10-03 22:17:34 +0900

    Minor fix

--- 2021/2106/draw_210627a.py
+++ 2021/2106/draw_210627a.py
@@ -30,26 +30,26 @@ for frame in tqdm(range(FRAMES)):
     noise2 = PerlinNoise(octaves=frame + 1, seed=30)
 
     for i in range(0, 2000):
-    
+
         if i == 0:
             path_below.moveTo((0, CANVAS))
             path_above.moveTo((0, 0))
-    
+
         if i == 1:
             x1, y1 = 0, 50
             x2, y2 = 0, 80
             path_below.lineTo((x1, y1))
             path_above.lineTo((x2, y2))
-    
+
         new_x1 = i / 360 * CANVAS
         new_y1 = 50 + noise1(i/1000) + 30 * math.sin(math.radians(i))
-    
+
         new_x2 = i / 360 * CANVAS
         new_y2 = 80 + noise2(i/60) + 30 * math.sin(math.radians(i))
-        
+
         path_below.lineTo((new_x1, new_y1))
         path_above.lineTo((new_x2, new_y2))
-    
+
         x1, y1 = new_x1, new_y1
         x2, y2 = new_x2, new_y2
 
@@ -62,8 +62,6 @@ for frame in tqdm(range(FRAMES)):
     wave = path_above.intersection(path_below)
 
     db.fill(1)
-
-    whole = db.BezierPath()
 
     db.translate(0, -1000)
     db.rotate(45)
